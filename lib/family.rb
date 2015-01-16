749b97cdba9ae89f6beb676e44dd6304202c781e
Will H McMahan <will@thoughtbot.com>
2015-01-16 16:14:00 -0500

    Add fellowly library

--- lib/family.rb
+++ lib/family.rb
@@ -1,13 +1,15 @@
 class Family
   SVG_BASE = "source/assets/components/"
 
-  attr_reader :name, :path, :repo_url, :description, :title
+  attr_reader :name, :path, :repo_url, :description, :title, :author_handle, :author_url
 
   def initialize(attributes)
     @title = attributes.fetch("title")
     @path = attributes.fetch("path")
     @repo_url = attributes.fetch("repo_url")
     @description = attributes.fetch("description")
+    @author_handle = attributes.fetch("author_handle")
+    @author_url = attributes.fetch("author_url")
   end
 
   def files
